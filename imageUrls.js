edf02fc28d31d909a9912cf659edb01fc6d27d00
swift-tech-35 <swifttech35@gmail.com>
2025-06-24 15:40:47 +0900

    Create imageUrls.js

---
+++ imageUrls.js
@@ -0,0 +1,10 @@
+// imageUrls.js
+window.env = {
+  BG: "https://drive.google.com/uc?export=view&id=1ib6xnYXe19z2e850jfOtehJONxNqPVmk",
+  TITLE_GRAD: "https://drive.google.com/uc?export=view&id=1Jz0EQ4lpcjJHIkFJDomyHddyrvFAlPPS",
+  TITLE_WHITE: "https://drive.google.com/uc?export=view&id=1_jOh1d-ix_ZFJ6-F1qkj-OBdLUqRAPSO",
+  SUBTITLE: "https://drive.google.com/uc?export=view&id=1bGF9t0u3Dqpjg85q_Zn5m1-JJ0sJ3td4",
+  CATCH: "https://drive.google.com/uc?export=view&id=1003gqKFw4CY99BuFX7X9LAXfDg__oB8K",
+  STONE: "https://drive.google.com/uc?export=view&id=1SYosanlvGbkh4JBUQurQNIVyW2Y44s_J",
+  BUTTON_TEXT: "https://drive.google.com/uc?export=view&id=1SYosanlvGbkh4JBUQurQNIVyW2Y44s_J",
+};
